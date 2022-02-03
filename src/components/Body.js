d15ebe9fc14e5e5c181858993b03a3c28fc97aa4
dimitermusic <dimiteryyordanov@gmail.com>
2022-02-03 13:31:47 -0800

    initial file structure

---
+++ src/components/Body.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import { FaInstagram } from 'react-icons/fa';
+import './Body.css'
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <h2>Summer 2022</h2>
+            {/* <a
+                target="_blank"
+                href="https://www.instagram.com/dimisongs/"
+                rel="noreferrer"
+            >
+                <span>
+                    <FaInstagram />
+                </span>
+            </a> */}
+        </div>
+    )
+}
+
+export default Body;
